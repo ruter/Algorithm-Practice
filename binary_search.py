9fb66dc6deb4229ac8e94a3ffbf447ef3ee5085e
Ruter <ruter.lu@gmail.com>
2016-07-30 21:30:02 +0800

    Add Binary Search

---
+++ binary_search.py
@@ -0,0 +1,20 @@
+def binSearch(arr, n, val):
+    left = 0
+    right = n - 1
+    while(left <= right):
+        middle = left + ((right - left) >> 1) # Faster than / 2
+        if arr[middle] > val:
+            right = middle - 1
+        elif arr[middle] < val:
+            left = middle + 1
+        else:
+            return middle
+    return -1
+
+# Test
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+val = 4
+
+result = binSearch(arr, len(arr), val)
+
+print result
